4ec932c0c150ed7c4d32014422004a5af8560e33
phamthainb <phamthai4643@gmail.com>
2019-11-24 23:43:17 +0700

    add base

--- src/views/components/layout/index.js
+++ src/views/components/layout/index.js
@@ -6,46 +6,7 @@ import routes from "@states/routes";
 
 
 class Layout extends Component {
-  componentDidMount() {
-    AOS.init({
-      duration: 500,
-      offset: 0,
-      delay: 0,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false
-    });
-    if ($(this).scrollTop() >= 42) {
-      $(".header-nav").addClass("scrolled");
-    } else {
-      $(".header-nav").removeClass("scrolled");
-    }
-    $(window).on("load", function () {
-      if ($(this).scrollTop() >= 42) {
-        $(".header-nav").addClass("scrolled");
-      } else {
-        $(".header-nav").removeClass("scrolled");
-      }
-    });
-    $(document).scroll(function () {
-      if ($(this).scrollTop() >= $("#main").offset().top) {
-        $(".back-top").addClass("active");
-      } else {
-        $(".back-top").removeClass("active");
-      }
-      if ($(this).scrollTop() > 42) {
-        $("#header").addClass("scrolled");
-      } else {
-        $("#header").removeClass("scrolled");
-      }
-    });
-    $(".back-top").on("click", function () {
-      $(".back-top").removeClass("active");
-      $("html, body").animate({
-        scrollTop: 0
-      }, 1000);
-    });
-  }
+  
   showRoutes = routes => {
     let result = null;
     if (routes.length > 0) {
